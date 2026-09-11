0b319faa29b4eb9ff7d258f6c016cdf434716a65
Yejun Guan <guanyejun79@gmail.com>
2026-09-11 21:48:57 +0100

    feat: add product catalogue foundation

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import { AppShell } from '../components/layout/AppShell.tsx'
+import { CataloguePage } from '../features/catalogue/CataloguePage.tsx'
 import { HomePage } from '../pages/HomePage.tsx'
 import { NotFoundPage } from '../pages/NotFoundPage.tsx'
 
@@ -8,6 +9,7 @@ export function AppRoutes() {
     <Routes>
       <Route element={<AppShell />}>
         <Route index element={<HomePage />} />
+        <Route path="catalogue" element={<CataloguePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
